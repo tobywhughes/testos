5fc5dd441d2cf207b7411c93b1856aa16808049c
Toby <tobywhughes@gmail.com>
2017-07-12 09:27:25 -0700

    Cleaned up repo/build process

--- grubconfig.sh
+++ grubconfig.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+mkdir -p isodir
+mkdir -p isodir/boot
+mkdir -p isodir/boot/grub
+ 
+cat > isodir/boot/grub/grub.cfg << EOF
+menuentry "myos" {
+	multiboot /boot/myos.kernel
+}
+EOF
 cp testos.bin isodir/boot/myos.bin
 grub-mkrescue -o testos.iso isodir
