35a6091d73e7a587245d2a5eb2bff17f865c7a18
Toby <tobywhughes@gmail.com>
2017-07-12 10:16:14 -0700

    Rearchitected directory format

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-i686-elf-as boot.s -o boot.o
+i686-elf-as kernel/arch/i386/boot.s -o boot.o
 echo "Assembly Compiled"
-i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -c kernel/kernel/kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 echo "Kernel Compiled"
-i686-elf-gcc -T linker.ld -o testos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+i686-elf-gcc -T kernel/arch/i386/linker.ld -o testos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
